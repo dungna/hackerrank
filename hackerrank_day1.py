bb8a6d52a376622083f71f659e110c7345e84092
Dung Nguyen <dungna@users.noreply.github.com>
2021-05-19 17:20:37 +0700

    Update hackerrank_day1.py

--- hackerrank_day1.py
+++ hackerrank_day1.py
@@ -1,3 +1,15 @@
+"""
+URL: https://www.hackerrank.com/challenges/correlation-and-regression-lines-6/problem
+Here are the test scores of 10 students in physics and history:
+Physics Scores  15  12  8   8   7   7   7   6   5   3
+History Scores  10  25  17  11  13  17  20  13  9   15
+Compute Karl Pearson’s coefficient of correlation between these scores.
+Compute the answer correct to three decimal places.
+Output Format
+In the text box, using the language of your choice, print the floating point/decimal value required. Do not leave any leading or trailing spaces.
+For example, if your answer is 0.255. In python you can print using
+"""
+
 # CACH 1
 from scipy.stats import pearsonr
 physics_scores =[15,12,8,8,7,7,7,6,5,3]
