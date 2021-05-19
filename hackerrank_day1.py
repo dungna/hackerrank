138d9c2f68854073f15b9666e6078f9d6d2a1bf7
Dung Nguyen <dungna@users.noreply.github.com>
2021-05-19 17:19:50 +0700

    Add files via upload

---
+++ hackerrank_day1.py
@@ -0,0 +1,48 @@
+# CACH 1
+from scipy.stats import pearsonr
+physics_scores =[15,12,8,8,7,7,7,6,5,3]
+history_scores = [10,25,17,11,13,17,20,13,9,15]
+corr,_ = pearsonr(physics_scores, history_scores)
+print('Pearsons correlation:%.3f' % corr)
+
+
+# CACH 2
+import math
+import numpy as np
+physics = [15, 12, 8, 8, 7, 7, 7, 6, 5, 3]
+history = [10, 25, 17, 11, 13, 17, 20, 13, 9, 15]
+p_mean = np.mean(physics)
+h_mean = np.mean(history)
+
+tu_so_physics = [p - p_mean for p in physics]
+tu_so_history = [h - h_mean for h in history]
+tu_so = 0
+for p, h in zip(tu_so_physics, tu_so_history):
+    tu_so += p*h
+
+mau_physics = sum([p**2 for p in tu_so_physics])
+mau_history = sum([h**2 for h in tu_so_history])
+mau_so = math.sqrt(mau_physics*mau_history)
+
+r = tu_so/mau_so
+print(round(r, 3))
+
+# CACH 3
+import math
+
+hs_score = [10, 25, 17, 11, 13, 17, 20, 13, 9, 15]
+ps_score = [15, 12, 8, 8, 7, 7, 7, 6, 5, 3]
+n = len(hs_score)
+total_1, total_2a, total_2b = 0, 0, 0
+
+avgHsScore = sum(hs_score)/n
+avgPsScore = sum(ps_score)/n
+
+for i in range(n):
+    total_1 += (hs_score[i] - avgHsScore) * (ps_score[i] - avgPsScore)
+    total_2a += (ps_score[i] - avgPsScore) * (ps_score[i] - avgPsScore)
+    total_2b += (hs_score[i] - avgHsScore) * (hs_score[i] - avgHsScore)
+
+r = round(total_1/(math.sqrt(total_2a*total_2b)), 3)
+
+print(r)
